37fbc3c46a337260abaf3258638dcd54fee136ca
Akibroszz <yenti.verle@outlook.com>
2021-01-28 22:00:27 +0100

    Add new keybinds

--- config.def.h
+++ config.def.h
@@ -18,7 +18,7 @@ static const char dmenufont[]       = "Hack Nerd Font:size=10:antialias=true:aut
 
 static const char col_bg[]                  = "#282a36";
 static const char col_fg[]                  = "#f8f8f2";
-static const char col_fg_focus[]            = "#8be9fd";
+static const char col_fg_focus[]            = "#ff79c6";
 static const char col_border[]              = "#44475a";
 static const char col_border_focus[]        = "#ff79c6";
 static const unsigned int baralpha = 0xEE;
@@ -46,7 +46,8 @@ static const Rule rules[] = {
 	/* class               instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",              NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",           NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "arcolinux-logout",  NULL,       NULL,       1 << 8,       1,           -1 },
+	{ "arcologout.py",     NULL,       NULL,       1 << 8,       1,           -1 },
+	{ "Arcolinux Logout",  NULL,       NULL,       1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
@@ -80,10 +81,19 @@ static const Layout layouts[] = {
 /* component of dmenucmd, manipulated in spawn() */
 static char dmenumon[2] = "0"; 
 
-static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "Run: ", NULL };
 static const char *termcmd[]  = { terminal, NULL };
 static const char *browsercmd[] = { browser, NULL };
 static const char *emacscmd[] = { "emacsclient", "-c", "-a", "emacs", NULL };
+
+static const char *raisevolumecmd[] = { "amixer", "set", "Master", "10%+", NULL };
+static const char *lowervolumecmd[] = { "amixer", "set", "Master", "10%-", NULL };
+static const char *muteaudiocmd[]  = { "amixer", "-D", "pulse", "set", "Master", "1+", "toggle", NULL };
+
+static const char *screenshotcmd[] = { "scrot", "'Screenshot-%Y-%m-%d-%H_%M.jpg'", "-q", "20", "&&", "mv", "$HOME/Pictures/Screenshots", NULL };
+
+/* A menu for logging out, hibernating, shutting of, ... */
+static const char *accountmanagercmd[] = { "arcolinux-logout", NULL };
 
 /* Scratchpad */
 static const char scratchpadname[] = "scratchpad";
@@ -93,57 +103,75 @@ static const char *scratchpadcmd[] = { terminal, "-t", scratchpadname, "-g", "12
 static char *statuscmds[] = { "notify-send Mouse$BUTTON" };
 static char *statuscmd[] = { "/bin/sh", "-c", NULL, NULL };
 
+#include <X11/XF86keysym.h>
+
 static Key keys[] = {
+	/* modifier                     key                         function        argument */
 	/* General Keys */
+	{ MODKEY,                       XK_Tab,                     view,           {0} },   
+	{ MODKEY,                       XK_b,                       togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_c,                       killclient,     {0} },
+	{ MODKEY,                       XK_x,                       spawn,          {.v = accountmanagercmd} },
+	{ MODKEY,                       XK_0,                       view,           {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,                       tag,            {.ui = ~0 } },
+	{ MODKEY|ControlMask,           XK_Return,                  zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,                       quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,                       quit,           {1} }, 
 
 	/* Layout Manipulation */
+	{ MODKEY,                       XK_j,                       focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,                       focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_i,                       incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_d,                       incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_h,                       setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_l,                       setmfact,       {.f = +0.05} },
+
+	{ MODKEY,                       XK_t,                       setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,                       setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,                       setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_g,                       setlayout,      {.v = &layouts[3]} },
+	{ MODKEY|ShiftMask,             XK_f,                       togglefullscr,  {0} },
+
+	{ MODKEY|ShiftMask,             XK_comma,                   cyclelayout,    {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,                  cyclelayout,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_j,                       rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,                       rotatestack,    {.i = -1 } },
+	{ MODKEY,                       XK_space,                   setlayout,      {0} },
+	{ MODKEY|ShiftMask,             XK_space,                   togglefloating, {0} },
 
 	/* MOD + Key Applications */
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_F1,     spawn,          {.v = browsercmd } },
-	{ MODKEY,                       XK_F2,     spawn,          {.v = emacscmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_Return,                  spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_F1,                      spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_F2,                      spawn,          {.v = emacscmd } },
+	{ MODKEY,                       XK_s,                       togglescratch,  {.v = scratchpadcmd } },
 
 	/* MOD + Shift + Key Applications */
+	{ MODKEY|ShiftMask,             XK_Return,                  spawn,          {.v = dmenucmd } },
 
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-//	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY|ControlMask,		    XK_comma,  cyclelayout,    {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_s,      togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_r,      quit,           {1} }, 
+	/* Audio */
+	{ 0,                            XF86XK_AudioMute,		    spawn,	        {.v = muteaudiocmd } },	
+	{ 0,                            XF86XK_AudioRaiseVolume,	spawn,		    {.v = raisevolumecmd } },
+	{ 0,                            XF86XK_AudioLowerVolume,	spawn,		    {.v = lowervolumecmd } },
+
+	/* Screenshots */
+	{ 0,                            XK_Print,                   spawn,          {.v = screenshotcmd } },
+
+
+	/* Switching between monitors */
+	{ MODKEY,                       XK_comma,                   focusmon,       {.i = -1 } },
+	{ MODKEY,                       XK_period,                  focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,                   tagmon,         {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,                  tagmon,         {.i = +1 } },
+
+	TAGKEYS(                        XK_1,                                       0)
+	TAGKEYS(                        XK_2,                                       1)
+	TAGKEYS(                        XK_3,                                       2)
+	TAGKEYS(                        XK_4,                                       3)
+	TAGKEYS(                        XK_5,                                       4)
+	TAGKEYS(                        XK_6,                                       5)
+	TAGKEYS(                        XK_7,                                       6)
+	TAGKEYS(                        XK_8,                                       7)
+	TAGKEYS(                        XK_9,                                       8)
 };
 
 /* button definitions */
